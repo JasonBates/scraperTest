695b3ceaba1b326ab75963f7e37ba3dfcf88a192
Jason Bates <jason@jasonbates.com>
2013-11-17 21:41:41 +0000

    scraping yCombinator news

---
+++ scrape.js
@@ -0,0 +1,14 @@
+// scrape.js - Cheerio and request
+
+var request = require('request');
+var cheerio = require('cheerio');
+
+request('https://news.ycombinator.com', function (error, response, html) {
+  if (!error && response.statusCode == 200) {
+    var $ = cheerio.load(html);
+    $('td.title a').each(function(i, element){
+      var a = $(element);
+      console.log(i+1 + " " + a.text());
+    });
+  }
+});
